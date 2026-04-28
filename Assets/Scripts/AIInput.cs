6ceccc30dfbbf1c1fa0ce9cbec979bb3200d14bf
bcgtrunner <ilya.timakov24@gmail.com>
2026-04-28 09:20:19 +0700

    Cube AI shoot

--- Assets/Scripts/AIInput.cs
+++ Assets/Scripts/AIInput.cs
@@ -8,25 +8,29 @@ public class AIInput : MonoBehaviour
     [SerializeField] private float _maxWaitTime = 2.0f;
 
     private PlayerController _controller;
-    private bool _isThinking = false;
+    private PaintShooter _shooter;
+    private bool isLaunching = false;
+    private bool isShooting = false;
+    public PlayerController Target;
 
     private void Awake()
     {
         _controller = GetComponent<PlayerController>();
+        _shooter = GetComponent<PaintShooter>();
     }
 
     private void Update()
     {
-        if (_controller.IsAttached && !_isThinking)
+        if (_controller.IsAttached && !isLaunching && !isShooting)
         {
             StartCoroutine(WaitAndLaunch());
+            StartCoroutine(WaitAndShoot());
         }
     }
 
     private IEnumerator WaitAndLaunch()
     {
-        _isThinking = true;
-
+        isLaunching = true;
         float waitTime = Random.Range(_minWaitTime, _maxWaitTime);
         yield return new WaitForSeconds(waitTime);
 
@@ -36,7 +40,23 @@ public class AIInput : MonoBehaviour
 
             _controller.TryLaunch(randomDir);
         }
+        isLaunching = false;
+    }
 
-        _isThinking = false;
+    private IEnumerator WaitAndShoot()
+    {
+        isShooting = true;
+        float waitTime = Random.Range(_minWaitTime, _maxWaitTime) * 5;
+        yield return new WaitForSeconds(waitTime);
+
+        if (_controller.IsAttached)
+        {
+            Vector3 randomDir = Random.onUnitSphere;
+            if (Vector3.Dot(randomDir, Target.transform.position - transform.position) > 0)
+            {
+                _shooter.TryShoot(randomDir);
+            }
+        }
+        isShooting = false;
     }
 }
