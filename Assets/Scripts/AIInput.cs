a7e9b3741d71b16f66a983832b839071ac281ab2
yurich <urvancevurij24@gmail.com>
2026-04-28 16:22:21 +0700

    Add dynamic bot room spawning

--- Assets/Scripts/AIInput.cs
+++ Assets/Scripts/AIInput.cs
@@ -6,17 +6,30 @@ public class AIInput : MonoBehaviour
 {
     [SerializeField] private float _minWaitTime = 0.5f;
     [SerializeField] private float _maxWaitTime = 2.0f;
+    [SerializeField] private float _launchAimJitter = 0.35f;
 
     private PlayerController _controller;
     private PaintShooter _shooter;
     private bool isLaunching = false;
     private bool isShooting = false;
     public PlayerController Target;
+    public event System.Action<AIInput> Destroyed;
 
     private void Awake()
     {
         _controller = GetComponent<PlayerController>();
         _shooter = GetComponent<PaintShooter>();
+    }
+
+    public void LaunchImmediately()
+    {
+        if (!_controller.IsAttached)
+        {
+            return;
+        }
+
+        Vector3 launchDirection = GetImmediateLaunchDirection();
+        _controller.TryLaunch(launchDirection);
     }
 
     private void Update()
@@ -36,9 +49,7 @@ public class AIInput : MonoBehaviour
 
         if (_controller.IsAttached)
         {
-            Vector3 randomDir = Random.onUnitSphere;
-
-            _controller.TryLaunch(randomDir);
+            _controller.TryLaunch(GetImmediateLaunchDirection());
         }
         isLaunching = false;
     }
@@ -49,7 +60,7 @@ public class AIInput : MonoBehaviour
         float waitTime = Random.Range(_minWaitTime, _maxWaitTime) * 5;
         yield return new WaitForSeconds(waitTime);
 
-        if (_controller.IsAttached)
+        if (_controller.IsAttached && Target != null)
         {
             Vector3 randomDir = Random.onUnitSphere;
             if (Vector3.Dot(randomDir, Target.transform.position - transform.position) > 0)
@@ -58,5 +69,31 @@ public class AIInput : MonoBehaviour
             }
         }
         isShooting = false;
+    }
+
+    private void OnDestroy()
+    {
+        Destroyed?.Invoke(this);
+    }
+
+    private Vector3 GetImmediateLaunchDirection()
+    {
+        if (Target != null)
+        {
+            Vector3 towardTarget = (Target.transform.position - transform.position).normalized;
+            if (towardTarget.sqrMagnitude > Mathf.Epsilon)
+            {
+                Vector3 randomOffset = Random.onUnitSphere * _launchAimJitter;
+                Vector3 biasedDirection = (towardTarget + randomOffset).normalized;
+                if (biasedDirection.sqrMagnitude > Mathf.Epsilon)
+                {
+                    return biasedDirection;
+                }
+
+                return towardTarget;
+            }
+        }
+
+        return Random.onUnitSphere;
     }
 }
