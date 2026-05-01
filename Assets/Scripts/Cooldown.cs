55a1fb61cf9e8c4dadeb7bded0a8578795b5a436
yurich <urvancevurij24@gmail.com>
2026-05-01 17:19:48 +0700

    swap left/right click, rate-limited scroll wheel with shot queue

---
+++ Assets/Scripts/Cooldown.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Cooldown
+{
+    private readonly float _duration;
+    private float _lastReset;
+
+    public Cooldown(float duration)
+    {
+        _duration = duration;
+        _lastReset = -duration;
+    }
+
+    public void Reset()
+    {
+        _lastReset = Time.time;
+    }
+
+    public bool Over()
+    {
+        return Time.time - _lastReset >= _duration;
+    }
+}
