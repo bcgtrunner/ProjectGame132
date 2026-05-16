ecd10b9221e73558714f56269d97e45e29592422
bcgtrunner <210253731+bcgtrunner@users.noreply.github.com>
2026-05-16 13:50:04 +0700

    Wall animation

--- Assets/Scripts/Paint.cs
+++ Assets/Scripts/Paint.cs
@@ -5,13 +5,14 @@ public class Paint : MonoBehaviour
 {
     private Wall attachedWall;
     private readonly HashSet<PlayerController> _touching = new();
+    [SerializeField] private Collider _collider;
 
     public void AttachTo(Wall wall)
     {
         if (wall == null) Destroy(gameObject);
         attachedWall = wall;
         transform.SetParent(wall.transform);
-        wall.Destroyed += Destroy;
+        attachedWall.Destroyed += () => _collider.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
